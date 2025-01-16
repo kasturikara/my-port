e3508bb50474586bedf343e8a3b85b34bb824c18
kasturikara <littlecommander46@gmail.com>
2025-01-16 11:26:12 +0700

    Add utility functions, theme provider, and hero component for portfolio site

---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./MagicButton";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div className="">
+        <Spotlight
+          className="h-screen -top-40 -left-10 md:-left-32 md:-top-20"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+
+      {/* header */}
+      <div className="relative z-10 flex justify-center my-20">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="text-xs tracking-widest text-center text-blue-100 uppercase max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless User Experiences"
+          />
+
+          <p className="mb-4 text-sm text-center md:tracking-wider md:text-lg lg:text-2xl">
+            Hi, I&apos;m Aditya, a Fullstacks Developer with a passion for web
+            development.
+          </p>
+
+          <a href="#about">
+            <MagicButton />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
